82c606e816cd69f27b5c0c2feb6ebe3a4006f0cc
rafliramadhan <rafliugm222@gmail.com>
2025-06-19 11:53:46 +0700

    feat: add new folder

---
+++ primitive_data_type/target/debug/deps/app-759e163b8cf79ace.d
@@ -0,0 +1,5 @@
+/home/rafliramadhan/Documents/GitHub/basic-rust/primitive_data_type/target/debug/deps/app-759e163b8cf79ace: main.rs
+
+/home/rafliramadhan/Documents/GitHub/basic-rust/primitive_data_type/target/debug/deps/app-759e163b8cf79ace.d: main.rs
+
+main.rs:
